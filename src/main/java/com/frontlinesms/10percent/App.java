1396713c1825831bea2197e9ce6a581d3353007a
Jimmy Mbiyu <jimmy@frontlinesms.com>
2014-11-14 10:10:22 +0300

    Added Java project structure

---
+++ src/main/java/com/frontlinesms/10percent/App.java
@@ -0,0 +1,13 @@
+package com.frontlinesms.10percent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
